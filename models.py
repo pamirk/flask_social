ef98b8eabcf0ab012d5ca14c1ccc75bee7ab3a0f
Pamir khan <pmysteryk@gmail.com>
2023-06-29 06:11:01 +0500

    feat: added create_user class method

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 import datetime
-
+from flask_login import UserMixin
 from peewee import *
+from werkzeug.security import generate_password_hash
 
 DATABASE = SqliteDatabase('social.db')
 
 
-class User(Model):
+class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=100)
@@ -15,3 +16,15 @@ class User(Model):
     class Meta:
         database = DATABASE
         order_by = ('-joined_at',)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            with DATABASE.transaction():
+                cls.create(
+                    username=username,
+                    email=email,
+                    password=generate_password_hash(password),
+                    is_admin=admin)
+        except IntegrityError:
+            raise ValueError("User already exists")
