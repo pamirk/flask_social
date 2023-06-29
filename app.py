46781a3fe530e46baad444f1c345a26a990282b3
Pamir khan <pmysteryk@gmail.com>
2023-06-29 21:19:51 +0500

    feat: added LoginManager to set up our application
    so we can load users

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask, g
-from flask_login import current_user
+from flask_login import current_user, login_manager, LoginManager
 
 import models
 
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
 app = Flask(__name__)
+app.secret_key = 'dsdssh.bouoastwuh.4343>?*;:s,wddsds!'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 
 @app.before_request
@@ -27,4 +44,14 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    models.initialize()
+    try:
+        models.User.create_user(
+            username='pamir',
+            email='pamir.khan11@gmail.com',
+            password='password',
+            admin=True
+        )
+    except ValueError:
+        pass
+    app.run(debug=DEBUG, host=HOST, port=PORT)
