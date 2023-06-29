a4b45ddc13bb9d9b52dee04ac9a21f3fc78e3c55
Pamir khan <pmysteryk@gmail.com>
2023-06-29 06:46:10 +0500

    feat: added before_request and after_request functions

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, g
+from flask_login import current_user
+
+import models
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
 
 
 @app.route('/')
