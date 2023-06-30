a000a71205a73e56371f07eb605a41ec8ca61614
Pamir khan <pmysteryk@gmail.com>
2023-06-30 11:31:29 +0500

    feat: added username exists validator

--- forms.py
+++ forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Regexp, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Regexp, Email, Length, EqualTo, ValidationError
+
+from models import User
+
+
+def name_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('User with that name already exists.')
 
 
 class RegisterForm(FlaskForm):
