a78b0d4289711a989f7a40f3485c47952309fea1
Pamir khan <pmysteryk@gmail.com>
2023-06-30 14:30:37 +0500

    feat: added login view function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, g, flash, redirect, url_for, render_template
-from flask_login import current_user, login_manager, LoginManager
+from flask_login import current_user, login_manager, LoginManager, login_user
+from werkzeug.security import check_password_hash
 
 import forms
 import models
@@ -51,6 +52,24 @@ def register():
         )
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash("Your email or password doesn't match!", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You've been logged in!", "success")
+                return redirect(url_for('index'))
+            else:
+                flash("Your email or password doesn't match!", "error")
+    return render_template('login.html', form=form)
 
 
 @app.route('/')
